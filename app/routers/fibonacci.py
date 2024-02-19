0994813e491670d73aa8f03b431df5e5229eb031
alex-tiulip <aleksandr.tiulpanov@cellpointdigital.com>
2024-02-19 02:32:12 +0100

    Add gmpy2 to enhance computation

--- app/routers/fibonacci.py
+++ app/routers/fibonacci.py
@@ -12,9 +12,12 @@ router = APIRouter(
 async def FibValueForNumber(number: int):
     try:
         result = computeFibonacciTask.delay(number)
-        return jsonable_encoder({'input': number, 'task_id': result.id}), 202
+        return jsonable_encoder({'result': result.get()}), 200
     except ValueError:
         return jsonable_encoder({'error': 'Invalid input. Please provide a valid integer.'}), 400
+    except Exception as e:
+        return jsonable_encoder({'error': 'Unexpected error!'}), 400
+
 
 
 @router.get("/count/from1toN/{number}")
--- app/worker/tasks/compute_fibonacci_task.py
+++ app/worker/tasks/compute_fibonacci_task.py
@@ -1,9 +1,13 @@
 from .. import celery_app
-
+import gmpy2 
 
 @celery_app.task
 def computeFibonacciTask(n):
     if n <= 1:
         return n
-    else:
-        return computeFibonacciTask(n - 1) + computeFibonacciTask(n - 2)
+    
+    fib_values = [gmpy2.mpz(0), gmpy2.mpz(1)]
+    for i in range(2, n + 1):
+        fib_values.append(gmpy2.mpz(fib_values[i - 1] + fib_values[i - 2]))
+
+    return str(fib_values[n])
